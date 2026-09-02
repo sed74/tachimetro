69de1e289f190358a21d1a02b0377f7dd6db36d0
Federico Marchesi <fede.sed74@gmail.com>
2026-09-02 19:27:26 +0200

    chore(config): bump versionCode to 3 and versionName to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.sed.tachimetro"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
